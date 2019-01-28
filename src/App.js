72ccf1c1df30394da06d8592133f735ee68458e5
Bahsa31 <44473240+Bahsa31@users.noreply.github.com>
2019-01-28 20:54:23 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,22 +2,50 @@ import React, { Component } from 'react';
 import Users from './User/Users';
 import Person from './Person/Person';
 import Propsex from './Propsex/Propsex';
-import logo from './logo.svg';
+import Propsag from './Propsag/Propsag';
 import './App.css';
 
 
 class App extends Component {
+  state={
+    propsag:[
+     {name:'Shaik',company:'Swiggy'},
+     {name:'Khadar',company:'Swiggy'},
+     {name:'Basha',company:'Swiggy'}
+    ]
+    }
+    switchNameHandler = () => {
+      //console.log('Was Clicked!');
+      this.setState({
+        propsag:[
+          {name:'Shaik',company:'Swiggy_XL'},
+          {name:'Khadar',company:'Swiggy'},
+          {name:'Basha',company:'Swiggy_Merch'}
+        ]});
+    }
   render() {
     return (
       <div className='App'>
         <h1>JavaScript Library</h1>    
-        <h1> <Users /></h1>
-        <Person />
-        <Person />
-        <Person />
-        <Propsex name="Shaik" />
-        <Propsex name="Khadar">Working in Swiggy</Propsex>
+
+        {/* Adding components */}
+        <h1> <Users /></h1> 
+        <Person />   
+        <Person />  
+        <Person />  
+
+        {/*Props example */ }
+        <Propsex name="Shaik" /> 
+        <Propsex name="Khadar">Working in Swiggy</Propsex> 
         <Propsex name="Basha" />
+
+        {/*  State Manipulation example */}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+
+        {/*  State example */}
+        <Propsag name={this.state.propsag[0].name} company={this.state.propsag[0].company}/>
+        <Propsag name={this.state.propsag[1].name} company={this.state.propsag[1].company}/>
+        <Propsag name={this.state.propsag[2].name} company={this.state.propsag[2].company}/>
       </div>
       
       )
---
+++ src/Propsag/Propsag.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Propsag = (Props)=>{
+    return (
+        <div>
+        <h1>My name is {Props.name}, working in {Props.company}</h1>
+       </div> 
+    );
+}
+
+export default Propsag;
